8062f9a834d7412063f9780e2f4da32550b65cca
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-08-22 23:11:29 -0400

    resume done

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -46,7 +46,6 @@ export default class extends React.Component {
     ).then(res => {
       console.log('Email successfully sent!')
     })
-    // Handle errors here however you like, or use a React error boundary
     .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
   }
 }
