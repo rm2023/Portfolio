fbc152b2a50f0fe4d403c06420ca574b193cb963
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-08-22 21:56:07 -0400

    fixing messenger

--- portfolio/src/components/pages/Form.js
+++ portfolio/src/components/pages/Form.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import '../css/contact.css';
-// import { MdOutlineEmail } from "react-icons/md"
-// import { FaGithub } from "react-icons/fa"
-// import { BsLinkedin } from "react-icons/bs"
 
 export default class extends React.Component {
   constructor(props) {
@@ -11,36 +7,39 @@ export default class extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   render() {
     return (
       <form className="test-mailing">
-      <h1>Let's see if it works</h1>
-      <div>
-        <textarea
-          id="test-mailing"
-          name="test-mailing"
-          onChange={this.handleChange}
-          placeholder="Send me a message"
-          required
-          value={this.state.feedback}
-          style={{ width: '100%', height: '150px' }}
-        />
-      </div>
-      <input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
-    </form>
+        <h1>Let's see if it works</h1>
+        <div>
+          <textarea
+            id="test-mailing"
+            name="test-mailing"
+            onChange={this.handleChange}
+            placeholder="Post some lorem ipsum here"
+            required
+            value={this.state.feedback}
+            style={{ width: '100%', height: '150px' }}
+          />
+        </div>
+        <input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
+      </form>
     )
   }
+
   handleChange(event) {
     this.setState({ feedback: event.target.value })
-  }handleSubmit(event) {
-    const templateId = 'template_vbyz2yj';
+  }
+
+  handleSubmit(event) {
+    const templateId = 'template_id';
     this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
   }
   
   sendFeedback (templateId, variables) {
     window.emailjs.send(
-      'template_vbyz2yj',
+      'gmail',
       templateId,
       variables
     ).then(res => {
@@ -50,4 +49,3 @@ export default class extends React.Component {
     .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
   }
 }
-
